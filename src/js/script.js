e98935825d7a4fccc2a1c5944dafb74ea01d6d6e
najmul-eimon <najmul.eimon@intelli.global>
2023-03-29 17:27:46 +0600

    home page end

--- src/js/script.js
+++ src/js/script.js
@@ -14,10 +14,43 @@ $(function(){
   $(window).on('resize', function(){
     if(window.innerWidth >= 768){
       $('.mobile-menu').removeClass('hidden').removeAttr("style").show();
-      console.log('hi');
     }
     else{
       $('.mobile-menu').addClass('hidden');
     }
   });
+
+  /*======================= project slider ========================*/
+  var projectSwiper = new Swiper(".project-slider", {
+    spaceBetween: 24,
+    grabCursor: false,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: true,
+    speed: 3000,
+    allowTouchMove:true,
+    freeModeMomentum: false,
+    breakpoints: {
+      576: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 2,
+      }
+    },
+    pagination: {
+      el: ".project-pagination",
+      clickable: true,
+    },
+  });
+
+  /*======================= Counter up ========================*/
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
 });
