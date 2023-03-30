56fe3ef9e74c3341ce83fe183acdf589251e1f10
najmul-eimon <najmul.eimon@intelli.global>
2023-03-30 17:20:52 +0600

    service page added

--- src/js/script.js
+++ src/js/script.js
@@ -53,4 +53,20 @@ $(function(){
     time: 1000
   });
 
+  /*======================= service tab ========================*/
+  $('.tabs-nav li:first-child').addClass('active');
+  $('.tab-content').hide();
+  $('.tab-content:first').show();
+
+  // Click function
+  $('.tabs-nav li').click(function(){
+    $('.tabs-nav li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+  });
+
 });
